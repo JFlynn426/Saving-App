d117fc18110b0e101e31397b925b7388b5f11e09
Jflynn426 <jflynnpics@gmail.com>
2019-02-18 17:28:27 -0500

    linux subsystem

--- Models/SavingGoal.cs
+++ Models/SavingGoal.cs
@@ -1,1 +1,10 @@
-namespace 
+namespace saving_app.Models
+{
+    public class SavingGoal
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Goal { get; set; }
+        public int Saved { get; set; }
+    }
+}
