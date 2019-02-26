5f3b4ca22ca543a0e12089fd058c2ef6676a1d36
Jflynn426 <jflynnpics@gmail.com>
2019-02-26 06:37:41 -0500

    edited context for deployment???

--- Migrations/20190226015935_added context.Designer.cs
+++ Migrations/20190226015935_added context.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using saving_app;
@@ -9,9 +10,10 @@ using saving_app;
 namespace SavingApp.Migrations
 {
     [DbContext(typeof(SavingGoalContext))]
-    partial class SavingGoalContextModelSnapshot : ModelSnapshot
+    [Migration("20190226015935_added context")]
+    partial class addedcontext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20190226015935_added context.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SavingApp.Migrations
+{
+    public partial class addedcontext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Saved",
+                table: "SavingGoal",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Goal",
+                table: "SavingGoal",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Saved",
+                table: "SavingGoal",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Goal",
+                table: "SavingGoal",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
